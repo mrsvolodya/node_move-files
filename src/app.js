da49a56ab634dc57e24b679c52e2e587aba8aa7a
mrsvolodya <vovamursky@gmail.com>
2024-09-27 20:09:23 +0300

    pre-commit

--- src/app.js
+++ src/app.js
@@ -1,22 +1,83 @@
+/* eslint-disable no-console */
 const fs = require('fs');
-const [source, destination] = process.argv.slice(-2);
+const path = require('path');
+const [s, d] = process.argv.slice(2);
+// let des;
 
-const isSourse = fs.existsSync(source);
+// const extNm = source.split('/').slice(-1).join('');
+// const extension = path.extname(s);
+// const slash = destination.lastIndexOf('/') === destination.length - 1;
 
-const isDirectory = fs.stat(destination, (_error, stats) => {
-  return stats.isDirectory();
-});
-// const isDestination = fs.existsSync(destination);
-// const isTrue = destination.includes('.');
+try {
+  const source = path.resolve(s);
+  const destination = path.resolve(d);
 
-// console.log('🚀 ~ isTrue:', isTrue);
+  const isExist = fs.existsSync(source);
 
-if (process.argv.length !== 4) {
-  console.error('Error path');
-} else if (!isSourse) {
-  console.error('Not exist such source');
-} else if (source !== destination && !isDirectory) {
-  fs.renameSync(source, destination, (error) => {
-    console.error(error);
-  });
+  console.log('🚀 ~ isExist:', isExist);
+
+  if (!isExist) {
+    throw new Error('No exist source');
+  }
+
+  const isDirectory = fs.lstatSync(destination).isDirectory();
+
+  if (isDirectory && fs.existsSync(destination)) {
+    const fileName = path.basename(source);
+    const joinPath = path.join(destination, fileName);
+
+    fs.renameSync(source, joinPath);
+  } else {
+    const dirname = path.dirname(destination);
+
+    if (!fs.existsSync(dirname)) {
+      throw new Error('No exist destination');
+    }
+    fs.renameSync(source, destination);
+  }
+} catch (e) {
+  console.error(e.message);
 }
+
+// const newPathWithSlash = path.join(destination + extNm);
+// const newPathWithoutSlash = path.join(destination + extension);
+
+// console.log(
+//   'NONAME',
+//   {
+//     extNm,
+//     extension,
+//     isDir: !!isDir,
+//     isFile,
+//     slash,
+//     newPathWithSlash,
+//     newPathWithoutSlash,
+//   },
+//   slash,
+// );
+// fs.renameSync(source, destination, (error) => {
+//   console.error(error);
+// });
+// try {
+//   if (process.argv.length !== 4) {
+//     console.error('Less PARAMETERS');
+//   } else if (source !== destination) {
+//     fs.renameSync(source, destination, (error) => {
+//       console.error(error);
+//     });
+//   }
+// } catch (e) {
+//   console.error(e);
+// }
+
+// try {
+//   fs.lstatSync(destination.split('/').slice(0, -1).join('/')).isDirectory();
+// } catch (e) {
+//   console.error('ERRRRRROR');
+// }
+
+// else if (!slash && source !== destination) {
+//   const newPath = path.join(destination + extension);
+
+//   fs.moveSync(source, newPath);
+// }
